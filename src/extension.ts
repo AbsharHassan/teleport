e0c95374d859361956fc68ce4903af35b0310946
AbsharHassan <abshar.hassan7@gmail.com>
2024-01-30 05:42:20 +0500

    wip: declutter, remove unused vars, comments

--- src/extension.ts
+++ src/extension.ts
@@ -3,15 +3,6 @@ import * as vscode from 'vscode'
 export function activate(context: vscode.ExtensionContext) {
   console.log('extension activated')
 
-  const wholeLineDecoration = vscode.window.createTextEditorDecorationType({
-    isWholeLine: true,
-    after: {
-      contentText: 'this line is active',
-      margin: '0 0 0 20px',
-      color: '#3399FF',
-    },
-  })
-
   let currentLine: number
   let startPos: vscode.Position
   let endPos: vscode.Position
@@ -19,35 +10,45 @@ export function activate(context: vscode.ExtensionContext) {
 
   let prevLine: number
 
-  let prevRange: vscode.Range
-  let currentRange: vscode.Range
-  let flagChange: boolean
+  let customDecoration: vscode.TextEditorDecorationType
 
   vscode.workspace.onDidChangeTextDocument((event) => {
     prevLine = event.contentChanges[0].range.start.line
   })
 
   vscode.window.onDidChangeTextEditorSelection((event) => {
-    // if (event.textEditor === vscode.window.activeTextEditor) {
-    //   if (currentLine !== event.selections[0].active.line) {
-    //     currentLine = event.selections[0].active.line
-
-    //     startPos = new vscode.Position(currentLine, 0)
-    //     endPos = new vscode.Position(currentLine, 0)
-
-    //     range = new vscode.Range(startPos, endPos)
-
-    //     event.textEditor.setDecorations(wholeLineDecoration, [range])
-    //   }
-    // }
-
     if (event.textEditor === vscode.window.activeTextEditor) {
       if (currentLine !== event.selections[0].active.line) {
         currentLine = event.selections[0].active.line
 
+        console.log(Math.abs(currentLine - prevLine))
+
         if (Math.abs(currentLine - prevLine) > 3) {
+          startPos = new vscode.Position(currentLine, 0)
+          endPos = new vscode.Position(currentLine, 0)
+          range = new vscode.Range(startPos, endPos)
+
+          event.textEditor.setDecorations(
+            updateDecoration(`you came from line ${prevLine + 1}`),
+            [range]
+          )
         }
       }
     }
   })
+
+  const updateDecoration = (contentText: string) => {
+    customDecoration?.dispose()
+
+    customDecoration = vscode.window.createTextEditorDecorationType({
+      isWholeLine: true,
+      after: {
+        contentText,
+        margin: '0 0 0 20px',
+        color: '#3399FF',
+      },
+    })
+
+    return customDecoration
+  }
 }
