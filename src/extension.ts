8579b625f0a17c87eb7e7d157708aec00b2fa0d5
AbsharHassan <abshar.hassan7@gmail.com>
2024-01-30 06:27:34 +0500

    wip: move logic to only onDidChangeTextEditorSelection func

--- src/extension.ts
+++ src/extension.ts
@@ -12,26 +12,33 @@ export function activate(context: vscode.ExtensionContext) {
 
   let customDecoration: vscode.TextEditorDecorationType
 
-  vscode.workspace.onDidChangeTextDocument((event) => {
-    prevLine = event.contentChanges[0].range.start.line
-  })
+  //   vscode.workspace.onDidChangeTextDocument((event) => {
+  //     prevLine = event.contentChanges[0].range.start.line
+
+  //     if (Math.abs(currentLine - prevLine) > 3) {
+  //       customDecoration.dispose()
+  //     }
+  //   })
 
   vscode.window.onDidChangeTextEditorSelection((event) => {
     if (event.textEditor === vscode.window.activeTextEditor) {
       if (currentLine !== event.selections[0].active.line) {
         currentLine = event.selections[0].active.line
 
-        console.log(Math.abs(currentLine - prevLine))
-
         if (Math.abs(currentLine - prevLine) > 3) {
           startPos = new vscode.Position(currentLine, 0)
-          endPos = new vscode.Position(currentLine, 0)
+          endPos = new vscode.Position(currentLine, Number.MAX_VALUE)
           range = new vscode.Range(startPos, endPos)
 
           event.textEditor.setDecorations(
             updateDecoration(`you came from line ${prevLine + 1}`),
             [range]
           )
+
+          prevLine = currentLine
+        } else {
+          customDecoration?.dispose()
+          prevLine = currentLine
         }
       }
     }
@@ -42,10 +49,13 @@ export function activate(context: vscode.ExtensionContext) {
 
     customDecoration = vscode.window.createTextEditorDecorationType({
       isWholeLine: true,
+      backgroundColor: 'black',
+      cursor: 'pointer',
       after: {
         contentText,
         margin: '0 0 0 20px',
         color: '#3399FF',
+        backgroundColor: 'black',
       },
     })
 
