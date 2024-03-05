0d7d88ec3fbe8856928c880777a3403f656ebc5d
AbsharHassan <abshar.hassan7@gmail.com>
2024-03-05 09:49:24 +0500

    wip: property to store character index

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,7 @@
 import { Range } from 'vscode'
 
-export type HistoryEntry = { range: Range; workingLine: number }
+export type HistoryEntry = {
+  range: Range
+  workingLine: number
+  workingCharacter?: number
+}
